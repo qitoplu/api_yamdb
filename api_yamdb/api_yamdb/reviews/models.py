ae0f24e326b4c309d9d72525fce41e9683f996f3
Александр Уланов <ulanov246@rambler.ru>
2023-03-29 23:30:57 +0300

    Create review model

---
+++ api_yamdb/api_yamdb/reviews/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class User():
+    pass
+
+class Title(models.Model):
+    """Модель произведения."""
+    pass
+
+class Review(models.Model):
+    """Модель отзыва."""
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True
+    )
+    text = models.TextField()
+    score = models.IntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(10, 'Оценка не может быть больше 10'),
+            MinValueValidator(1, 'Оценка не может быть меньше 1'),
+        ],
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
